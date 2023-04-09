b5cc85cfc8d75dca0d192ab18551707879bda78d
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-04-09 08:39:56 +0200

    Add files via upload

---
+++ 04-real-time-character-counter/app.js
@@ -0,0 +1,13 @@
+const textareaEl = document.getElementById('textarea');
+const totalCounterEl = document.getElementById('total-counter');
+let remainingCounterEl = document.getElementById('remaining-counter');
+
+textareaEl.addEventListener('keyup', ()=>{
+    updateCounter()
+});
+
+function updateCounter(){
+    totalCounterEl.innerText = textareaEl.value.length;
+    remainingCounterEl.innerText = textareaEl.getAttribute('maxlength') - textareaEl.value.length;
+}
+
