1df20f37edfc856e372c98395ed5a75e4c356bf9
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-04-10 21:54:17 +0200

    Add files via upload

---
+++ 15-auto-text-effect-animation/app.js
@@ -0,0 +1,24 @@
+const containerEl = document.querySelector('.container');
+
+const careers = ["YouTuber", "Web Developer", "Freelacer", "Instructor"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText(){
+    characterIndex++;
+    containerEl.innerHTML = `<h1>I am a ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+    if(characterIndex === careers[careerIndex].length){
+        careerIndex++;
+        characterIndex = 0;
+    }
+
+    if (careerIndex === careers.length) {
+        careerIndex = 0;
+      }
+    setTimeout(updateText, 400);
+}
+
